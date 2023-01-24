432495e5e85590d12769a76476c9a3186247b6d3
Koshoeva Ajar <ajarka1334@gmail.com>
2023-01-24 14:30:00 +0100

    added let with number

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let myName = "Ajara";
 let myFavoriteColor = "Blue";
 let myCity = "Karakol";
-let myDCoolNumber = 13;
+let myDCoolCake = "pie";
 let myCar = "BMW";
 let myMom = "Ainura";
 let myFood = "pizza";
 let myJuice = "Coke";
 let myFavoriteBook = "Agata Cristie";
-let myAge = 18;
+let myNumber = "secret";
 
+
+let myAge = 18;
+let myWeight = 52;
+let myHeight = 165;
+let myCoure = 3;
+let myFavoriteNumber = 13;
