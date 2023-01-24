3ad4c395024a60a53d9c3090b6fd4444d3e9b71d
Koshoeva Ajar <ajarka1334@gmail.com>
2023-01-24 14:54:46 +0100

    added ex- - 15 about number +-/*

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ let myFavoriteNumber = 13;
 
 let myDoorIsOpen = true;
 let myWindowsOpen = false;
+
+let example1 = 5 + 5;
+let example2 = 3 - 1;
+let example3 =( 2 + 12 ) * 5;
+let example4 = 3 * 3;
+let example5 = 9 / 1;
+let example6 = 3 - 0 + 5 / 2;
+let example7 = 6 / 2 + 2;
+let example8 = 12 + 12;
+let example9 = 23 ** 15 ;
+let example10 = ( 5 + 5 ) + 10;
+let example11 = 10 % 2 ;
+let example12 = 2.1 * 1.2;
+let example13 = 91 ** ( 1 / 2 ); //корень
+let example14 = 17 % 2; // остаток
+let example15 = 64 ** ( 1 / 3 );
+
+
+
+
+
